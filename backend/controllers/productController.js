eff5a5c74b4999340ab1afc0c97259de3d712956
andrei1b <andreibica12@gmail.com>
2024-01-23 16:09:29 +0200

    error fixed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,12 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
-const getProducts = async (res, req) => {
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
-};
+});
 
-const getProductsById = async (res, req) => {
+const getProductsById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     return res.json(product);
@@ -14,6 +14,6 @@ const getProductsById = async (res, req) => {
     res.status(404);
     throw new Error("Resource not found");
   }
-};
+});
 
 export { getProductsById, getProducts };
