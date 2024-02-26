e4f6c2252276f94fac5e222e0e04b7fa01d19cd7
andrei1b <andreibica12@gmail.com>
2024-02-26 16:44:12 +0200

    Auth middleware & endpoint DONE

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,24 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
+
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
