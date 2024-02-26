e329a648a20486a7b5c1ae0bbeeb18c4d7f12be2
andrei1b <andreibica12@gmail.com>
2024-02-26 17:43:43 +0200

    profile endpoints DONE

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     isAdmin: {
       type: Boolean,
       requierd: true,
-      defalt: false,
+      default: false,
     },
   },
   {
@@ -30,6 +30,15 @@ const userSchema = new mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
